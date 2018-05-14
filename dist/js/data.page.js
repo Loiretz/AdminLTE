8bb95f2ac9eb7786611c156c2c10bfe6ae059c7a
Projeto Transversal <projeto@latitude.unb.br>
2018-05-14 15:54:31 -0300

    o caio é um lindo. básico do js, chamadas no html e ajax

--- dist/js/data.page.js
+++ dist/js/data.page.js
@@ -1,5 +1,33 @@
+// carregarTabela();
+
+
+
 $("#meuBotao").click( function()
-  {
-    $('#myModal').modal('show');
-  }
-);
+{
+  $('#myModal').modal('show');
+//
+  $.ajax({
+    url: 'https://api.ieu.caiorondon.com.br/us/system/status',
+    type: 'GET',
+    dataType: 'text',
+    success: function (result) {
+      var resultado = JSON.parse(result);
+      console.log(resultado.type);
+      $("#nomeusuariomodal").html(resultado.type);
+    },
+    error: function (jqXHR, tranStatus, errorThrown) {
+      alert('Status: ' + jqXHR.status + ' ' + jqXHR.statusText + '. ' +
+      'Response: ' + jqXHR.responseText);
+    }
+  });
+// }
+
+  console.log({'chave': 'valor', 'chave2': 'valor2'});
+});
+
+$(".minhaClasse").click( function()
+{
+  console.log("Clicado pela classe");
+});
+
+// DOM -> Html
