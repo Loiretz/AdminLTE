9da1a0473fc4b53bdc18d2fc224bcebedffb0233
Felipe O <felipe_oap@hotmail.com>
2018-05-30 10:57:47 -0300

    mais correcoes

--- dist/js/data.page.js
+++ dist/js/data.page.js
@@ -38,8 +38,8 @@ $("#botaoModalInst").click( function()
 });
 
 $(".pop").on("click", function() {
-   $('#imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
-   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+   $('#imagepreview').attr('src', $(this).attr('src'));
+   $('#imagemodal').modal('show');
 });
 
 // DOM -> Html
--- dist/js/login.js
+++ dist/js/login.js
@@ -1,15 +1,14 @@
 //funcoes js que atendem ao POST de login em login.html5shiv
 
 $("#botaoLogin").click( function(){
-  console.log("cliquei");
-
   //verificar com o caio se essa estrutrura aqui ta correta.
-  var email = $("#email").val();
-  var password = $("#password").val();
-  var data = {email:email,password:password}
+  var vemail = $("#email").val();
+  var vpassword = $("#password").val();
+  var data = {email:vemail , password:vpassword}
 
   if (email !== "" || password !== "")
   {
+    console.log("TO RODANDO O IF");
     $.ajax({
         type: 'POST',
         url: 'https://api.ieu.caiorondon.com.br/as/auth/login',
@@ -20,20 +19,23 @@ $("#botaoLogin").click( function(){
           var msg = "";
           if(response == 1)
           console.log("terminei, tu ta logado e vou te mandar pra outra pagina");
-          window.location.replace= "tables/data.html";
+          //window.location= "tables/data.html";
           //inserir aqui redirecionamento de página,
           //indicar o token e guarda-lo (COMO???)
         }//fim do success
       }
     )//fim do $.ajax
-    console.log("terminei e tu nao ta logado");
+
     //so pra testes
-    window.location.replace= "tables/data.html";
+    //window.location= "tables/data.html";
 
 
   }// fim do if
+  else {
+    console.log("terminei e nao ta logado");
+  }
 
-})
+});
 
 
 // beforeSend: function()
