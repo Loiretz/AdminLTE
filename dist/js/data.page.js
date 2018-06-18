d2a4343a7ec737094b2fa99211cbcdf58e817690
Projeto Transversal <projeto@latitude.unb.br>
2018-06-18 14:13:33 -0300

    git add .

--- dist/js/data.page.js
+++ dist/js/data.page.js
@@ -1,47 +1,99 @@
 // Configuração da tabela
-
+// console.log(Cookies.get('admin-ieu-token'));
 // $(document).ready(function() {
-var table = $('#example1').DataTable({
-  ajax:"json.txt",
-  columns: [
-    { "data": "nome" },
-    { "data": "instituicao" },
-    { "data": "situacao" }
-  ],
-  order: [[2, "desc"]],// ordenando pelo elemento 2 (data:salary)
 
-  "createdRow": function ( row, data, index ) {
-    if ( data['situacao'].replace(/[\$,]/g, '') == 'Pendente ') {
-      $('td', row).eq(2).addClass('highlight');
+var usuarios = null;
+
+function search_user(cpf){
+  var i = 0;
+
+  for(i = 0; i < usuarios.length; i++){
+    if (usuarios[i].cpf == cpf){
+      return usuarios[i];
     }
-  }
+  };
+
+  return null;
+}
+
+
+var table = $('#example1').DataTable({
+
+  order: [[3, "desc"]],// ordenando pelo elemento 2 (data:salary)
+  //
+  // "createdRow": function ( row, data, index ) {
+  //   if ( data['situacao'].replace(/[\$,]/g, '') == 'Pendente ') {
+  //     //nao funciona $('td', row).eq(2).addClass('highlight');
+  //   }
+  // }
 });
 
+$.ajax({
+    //aqui da certo
+    url: 'https://api.ieu.caiorondon.com.br/as/students',
+    type: 'GET',
+    headers: {
+     "Authorization":"Bearer " + Cookies.get('admin-ieu-token')
+   },
+    success: function (result) {
+      console.log(result)
+      usuarios = result.data;
+      search_user('');
+      $(result.data).each(function( index, value ) {
+         table.row.add( [
+           value.cpf, value.name, value.university_id, value.status
+         ] ).draw(true);
+      });
+
+    },
+    error: function (jqXHR, tranStatus, errorThrown) {
+      alert('Status: ' + jqXHR.status + ' ' + jqXHR.statusText + '. ' +
+      'Response: ' + jqXHR.responseText);
+    }
+});
+
+
 $('#example1 tbody').on( 'click', 'tr', function () {
-  var data = table.row( this ).data();
-  //console.log(table.row( this ).data());
-  console.log(data['situacao']);
-  abrirModal(table.row(this).data());
+  // var user = table.row( this );
+  //console.log();
+  var user = search_user(table.row( this ).data()[0])
+  abrirModal(user);
+
+  // $('#botaoConfirma').on('click',function(){
+  //   data = "kct de agulha";
+  //   console.log(data);
+  //
+  //   var ajax = new XMLLHttpRequest();
+  //   ajax.open("POST","json.txt",true);
+  //   ajax.setRe
 } );
+
 
 $(document).ready(function() {
   var data = table.row( this ).data();
   console.log("eu existo");
-  //style.color = 'red';
 });
 
 function abrirModal(data){
-  $('#nomeusermodal').html(data["nome"]);
-  $('#idusermodal').html(data["matricula"]);
-  $('#schoolusermodal').html(data["instituicao"]);
-  $('#imgidusermodal').attr('src', data["foto_comprovante_matricula"]);
+  $('#nomeusermodal').html(data["name"]);
+  $('#idusermodal').html(data["student_id"]);
+  $('#schoolusermodal').html(data["university_id"]);
+  $('#imgidusermodal').attr('src', data["photo"]);
   $('#cpfusermodal').html(data["cpf"]);
-  $('#imgcpfusermodal').attr('src', data["foto_cpf"]);
+  $('#imgcpfusermodal').attr('src', data["photo"]);
   $('#emailusermodal').html(data["email"]);
-  $('#statususermodal').html(data["situacao"]);
+  $('#statususermodal').html(data["status"]);
 
-  // document.getElementById("nomeusermodal").innerHTML = data["id"];
   $('#myModal').modal('show');
+
+  var flag_hide = false;
+
+  if (data['status'] == "CONFIRMED"){
+    flag_hide = true;
+  }
+
+  $('#botaoTexto').hide(flag_hide);
+  $('#botaoConfirma').hide(flag_hide);
   // $('.modal-content').resizable({
   //     //alsoResize: ".modal-dialog",
   //     minHeight: 300,
@@ -56,39 +108,6 @@ function abrirModal(data){
   // });
 
 }
-
-//https://bootsnipp.com/snippets/PaoDr
-
-// Preenchimento da tabela via API
-
-// carregarTabela();
-// $("#meuBotao").click( function()
-// {
-//   //chamando pelo id
-//   $('#myModal').modal('show');
-//   // console.log("cliclado pelo Seletor");
-//
-//   $.ajax({
-//     //aqui da certo
-//     url: 'https://api.ieu.caiorondon.com.br/us/system/status',
-//     type: 'GET',
-//     dataType: 'text',
-//
-//     success: function (result) {
-//       var resultado = JSON.parse(result);
-//       $("#nomeusermodal").html(resultado.data);
-//       $("#cpfusermodal").html(resultado.type);
-//       $("#idususermodal").html(resultado.info);
-//       $
-//       //fim do aqui certo
-//     },
-//     error: function (jqXHR, tranStatus, errorThrown) {
-//       alert('Status: ' + jqXHR.status + ' ' + jqXHR.statusText + '. ' +
-//       'Response: ' + jqXHR.responseText);
-//     }
-//   });
-//  //
-// });
 
 $("#botaoTexto").click( function(){
   $("#modalTexto").modal('show');
